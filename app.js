cb6db29d274df509b9a88782bd44af85728ec3f3
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-13 18:13:32 +0400

    import other file and direct execute him

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 
+// import other file
+require('./utils');
+
+// write to file, averride all in file
 fs.writeFileSync('notes.txt', 'My name is Fuad Suleymanov!');
 
-fs.appendFileSync('notes.txt', 'I am a programmer.');
+// append to file
+fs.appendFileSync('notes.txt', ' I am a programmer.');
+
+console.log('Print from app.js file')
+
+
---
+++ utils.js
@@ -0,0 +1,1 @@
+console.log('Print from utils.js file');
