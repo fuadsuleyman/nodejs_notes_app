2ec06ca8f36e8cf964612e8a36d3c72e5e405144
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-14 18:38:22 +0400

    get user input and execute function with yargs package part1

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 const chalk = require('chalk');
 const getNote = require('./notes');
+const yargs = require('yargs');
 
-const command = process.argv;
 
-// run app with below, third word(Fuad) is user input
-// node app.js Fuad
-console.log(`User input: ${command[2]}`);
+// as bolow write in terminal we run below command's handlers
+// node app.js add - run add command handler and etc
+
+// add command
+yargs.command({
+    command: 'add',
+    describe: 'Add new note.',
+    handler: () =>{
+        console.log(chalk.green.bold('New note is added.'));
+    }
+});
+
+
+// remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Note is removed.',
+    handler: () =>{
+        console.log(chalk.red.bold('Note is removed.'));
+    }
+});
+
+// list command
+yargs.command({
+    command: 'list',
+    describe: 'Notes are listed.',
+    handler: () =>{
+        console.log(chalk.blue.bold('Notes are listed.'));
+    }
+});
+
+// read command
+yargs.command({
+    command: 'read',
+    describe: 'Note is readed.',
+    handler: () =>{
+        console.log(chalk.yellowBright.bold('Note is readed.'));
+    }
+});
+
+console.log(yargs.argv);
