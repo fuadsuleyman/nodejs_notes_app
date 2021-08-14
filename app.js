4d6996add3afab8034f7f50e3f5b20515b1a53cc
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-14 17:32:28 +0400

    simple way to get input from user look to app.js

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-const fs = require('fs');
-
-const notes = require('./notes');
-const validator = require('validator');
 const chalk = require('chalk');
-
 const getNote = require('./notes');
 
-const note = getNote();
+const command = process.argv;
 
-console.log(`From Note file getNote function: ${note}`);
-
-console.log(`Validate email: ${validator.isEmail('fuadpayme@mail.com')}`)
-console.log(`Validate url: ${validator.isURL('fuadpaymemailcom')}`)
-
-console.log(chalk.green.bold.inverse.italic('Success'));
-
-const warning = chalk.keyword('orange');
-
-console.log(warning('This is Error word!'));
-
-
-
+// run app with below, third word(Fuad) is user input
+// node app.js Fuad
+console.log(`User input: ${command[2]}`);
