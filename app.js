be656ffce3da72c50039bcebd646b98142c6696c
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-14 10:44:12 +0400

    add validator check isEmail and isURL

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
 const notes = require('./notes');
+const validator = require('validator');
 
 const getNote = require('./notes');
 
 const note = getNote();
 
 console.log(`From Note file getNote function: ${note}`);
+
+console.log(`Validate email: ${validator.isEmail('fuadpayme@mail.com')}`)
+console.log(`Validate url: ${validator.isURL('fuadpaymemailcom')}`)
+
 
 
