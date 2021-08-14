121062ea183260ea678ff27ff7dfd480da577ceb
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-14 15:35:50 +0400

    install chalk for nice terminal messages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const notes = require('./notes');
 const validator = require('validator');
+const chalk = require('chalk');
 
 const getNote = require('./notes');
 
@@ -11,6 +12,12 @@ console.log(`From Note file getNote function: ${note}`);
 
 console.log(`Validate email: ${validator.isEmail('fuadpayme@mail.com')}`)
 console.log(`Validate url: ${validator.isURL('fuadpaymemailcom')}`)
+
+console.log(chalk.green.bold.inverse.italic('Success'));
+
+const warning = chalk.keyword('orange');
+
+console.log(warning('This is Error word!'));
 
 
 
